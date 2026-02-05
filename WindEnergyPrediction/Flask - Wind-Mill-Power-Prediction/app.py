89841209f84f28f50f2a4ad09daec3430cc4de04
Nigel Chawanda <156796026+Achawanda@users.noreply.github.com>
2026-02-05 20:54:47 +0530

    Add files via upload

---
+++ WindEnergyPrediction/Flask - Wind-Mill-Power-Prediction/app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "baf6eec1-9fcf-4901-b714-8afbe71463a2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
